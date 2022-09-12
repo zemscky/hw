8d835acb14ad1bb489307223e3e27c0e40f1d81d
zemscky <aleksei.zemscky@yandex.ru>
2022-09-12 17:53:03 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Задача 2
+        System.out.println("Задача 2");
+        // Задача 3
+        System.out.println("Задача 3");
+    }
+}
