1b3e570be9f3940512c81ec88d402109f2b145ed
zemscky <aleksei.zemscky@yandex.ru>
2022-09-12 22:49:18 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,33 @@ public class Main {
         }
         // Задача 5
         System.out.println("Задача 5");
+        int monthNumber = 5;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+                break;
         }
+    }
 }
