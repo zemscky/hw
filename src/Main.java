5469c186776493b3bfa65578ddb031a1dae61f86
zemscky <aleksei.zemscky@yandex.ru>
2022-09-12 18:06:01 +0300

    Задача 1,2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Задача 1
         System.out.println("Задача 1");
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }else{
@@ -10,6 +10,16 @@ public class Main {
         }
         // Задача 2
         System.out.println("Задача 2");
+        int clientDeviceYear = 2013;
+        if (clientOS == 0 && clientDeviceYear == 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (clientOS == 0 && clientDeviceYear != 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }else if (clientOS == 1 && clientDeviceYear == 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
         // Задача 3
         System.out.println("Задача 3");
     }
